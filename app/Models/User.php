6903450088d2303fd47b8173c1c357115de369a7
ProgrammerJayT <theanthem8@gmail.com>
2024-04-06 00:22:33 +0200

    Add new fields to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'identity',
+        'role',
         'email',
         'password',
     ];
