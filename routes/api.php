fe568aab22aae69e2911f3cc7fac922c117e10c8
ProgrammerJayT <theanthem8@gmail.com>
2024-04-06 23:54:49 +0200

    Add login endpoint to API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +20,12 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'v1'], function () {
-    //
+
+    //Login endpoint
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
     Route::group(['middleware' => 'auth:sanctum'], function () {
-        
+
         //Verify token endpoint
         Route::get('verify-token', function () {
             return response()->noContent(200);
