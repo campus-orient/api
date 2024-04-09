06c87a50d9c458c8e7cf6c26e378554e163195a3
ProgrammerJayT <theanthem8@gmail.com>
2024-04-09 02:06:44 +0200

    Format json response

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,14 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->user_id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'type' => $this->type,
+            'email' => $this->email,
+        ];
     }
 }
