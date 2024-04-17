7be7b17300f8c9fe07766e1cee3e7926d36f5f09
ProgrammerJayT <theanthem8@gmail.com>
2024-04-17 09:59:27 +0200

    Add user seed data to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,25 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $administrators = array(
+            [
+                "name" => "Karabo",
+                "surname" => "Dotwana",
+                "email" => "karabo.dotwana@campusorient.co.za",
+                "password" => "password",
+                "type" => "admin"
+            ], [
+                "name" => "Yolanda",
+                "surname" => "Mbande",
+                "email" => "yolanda.mbande@campusorient.co.za",
+                "password" => "password",
+                "type" => "admin"
+            ]
+        );
+
+        foreach ($administrators as $key => $administrator) {
+            User::create($administrator);
+        }
     }
 }
