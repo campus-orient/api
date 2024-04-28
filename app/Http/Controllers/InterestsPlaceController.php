abaf2b9e3e0ef15152aa4cc2e03ddac60e013378
ProgrammerJayT <theanthem8@gmail.com>
2024-04-28 19:30:21 +0200

    Create places of interests entity

---
+++ app/Http/Controllers/InterestsPlaceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InterestsPlace;
+use App\Http\Requests\StoreInterestsPlaceRequest;
+use App\Http\Requests\UpdateInterestsPlaceRequest;
+
+class InterestsPlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInterestsPlaceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InterestsPlace $interestsPlace)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InterestsPlace $interestsPlace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInterestsPlaceRequest $request, InterestsPlace $interestsPlace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InterestsPlace $interestsPlace)
+    {
+        //
+    }
+}
