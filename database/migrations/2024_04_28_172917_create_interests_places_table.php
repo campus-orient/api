e6a8ac73bc69a5132589cab7c79fa4fa765ebb4a
ProgrammerJayT <theanthem8@gmail.com>
2024-05-16 10:37:50 +0200

    Update interests_places table migration to auto-increment interests_place_id

--- database/migrations/2024_04_28_172917_create_interests_places_table.php
+++ database/migrations/2024_04_28_172917_create_interests_places_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('interests_places', function (Blueprint $table) {
-            $table->integer('interests_place_id');
+            $table->integer('interests_place_id')->autoIncrement();
             $table->string('name');
             $table->string('description');
             $table->string('latitude');
