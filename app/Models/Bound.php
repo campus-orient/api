f37ffb3ec8c824e3af5f52b34d4e77215fcb81d4
ProgrammerJayT <theanthem8@gmail.com>
2024-05-26 15:25:05 +0200

    Refactor Bound model to use 'bound_id' as primary key and add fillable attributes

--- app/Models/Bound.php
+++ app/Models/Bound.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Bound extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = "bound_id";
+
+    protected $fillable = [
+        'interests_place_id',
+        'latitude',
+        'longitude',
+        'detour_forward',
+        'detour_left',
+        'detour_back',
+        'detour_right'
+    ];
 }
