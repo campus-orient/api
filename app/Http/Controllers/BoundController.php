5e29c854d203f3b89b451734343bec25868ccdf9
ProgrammerJayT <theanthem8@gmail.com>
2024-05-28 14:17:49 +0200

    Implement store method

--- app/Http/Controllers/BoundController.php
+++ app/Http/Controllers/BoundController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\BoundCollection;
+use App\Http\Resources\BoundResource;
 use App\Models\Bound;
 use App\Http\Requests\StoreBoundRequest;
 use App\Http\Requests\UpdateBoundRequest;
 use Illuminate\Http\Request;
+use Throwable;
 
 class BoundController extends Controller
 {
+    public $conflictingBounds;
+
     /**
      * Display a listing of the resource.
      */
@@ -31,33 +35,58 @@ class BoundController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreBoundRequest $request)
     {
+        $this->conflictingBounds = false;
+
         //
+        try {
+            $newBoundLatitude = $request->input('latitude');
+            $newBoundLongitude = $request->input('longitude');
+
+            $existingBounds = Bound::where('interests_place_id', '=', $request->input('interests_place_id'))->get();
+
+            // Check for conflicts with existing bounds
+            if (count($existingBounds) > 0) {
+
+                foreach ($existingBounds as $key => $existingBound) {
+
+                    $distance = self::calculateDistanceBetween($newBoundLatitude, $newBoundLongitude, $existingBound->latitude, $existingBound->longitude);
+
+                    if ($distance < 10) {
+                        $this->conflictingBounds = true;
+                        break; // Exit the loop once a conflict is found
+                    }
+                }
+            }
+
+            // Return error response if conflicting bounds exist
+            if ($this->conflictingBounds) {
+                // return response()->json(['message' => 'Conflicting bounds found.'], 400);
+                abort(409, 'Conflicting bounds detected. Bounds must be at least 5 meters apart');
+            }
+
+            // Create a new bound if no conflicts exist
+            $newBound = Bound::create([
+                'latitude' => $newBoundLatitude,
+                'longitude' => $newBoundLongitude,
+                'interests_place_id' => $request->input('interests_place_id')
+            ]);
+
+            // Return success response with the newly created bound
+            return response()->json(['bound' => new BoundResource($newBound)], 201);
+        } catch (\Throwable $th) {
+            // Handle exceptions and return error response
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Bound $bound)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Bound $bound)
     {
         //
     }
@@ -76,5 +105,14 @@ class BoundController extends Controller
     public function destroy(Bound $bound)
     {
         //
+    }
+
+    public function calculateDistanceBetween($lat1, $lng1, $lat2, $lng2)
+    {
+        //
+        return 6371 * 1000 * acos(
+            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($lng2) - deg2rad($lng1)) +
+                sin(deg2rad($lat1)) * sin(deg2rad($lat2))
+        );
     }
 }
